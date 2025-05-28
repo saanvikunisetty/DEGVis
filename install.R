099652a429cb6cac73323c94218a6ed352157eca
Saanvi Kunisetty <4saanvik@gmail.com>
2025-05-28 11:55:49 -0500

    Added R Package Installation Script for Project Dependencies

---
+++ install.R
@@ -0,0 +1,14 @@
+#install.packages("tidyverse")
+#install.packages("pheatmap")
+
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+
+#BiocManager::install("DESeq2")
+#BiocManager::install("airway", force = TRUE)
+
+#library(tidyverse)
+#library(DESeq2)
+#library(airway)
+#library(pheatmap)
+
