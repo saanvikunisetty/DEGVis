4f6e1c58692f0c596986342bfde678d4f7946f93
Saanvi Kunisetty <4saanvik@gmail.com>
2025-06-03 18:44:01 -0500

    Conducted Initial Inspection of Airway Dataset

--- degvis.R
+++ degvis.R
@@ -1,13 +1,21 @@
-install.packages("tidyverse")
-install.packages("pheatmap")
+#install.packages("tidyverse")
+#install.packages("pheatmap")
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
 
-BiocManager::install("DESeq2")
-BiocManager::install("airway", force = TRUE)
+#BiocManager::install("DESeq2")
+#BiocManager::install("airway", force = TRUE)
 
-library(tidyverse)
-library(DESeq2)
-library(airway)
-library(pheatmap)
+#library(tidyverse)
+#library(DESeq2)
+#library(airway)
+#library(pheatmap)
+
+data("airway")
+library(SummarizedExperiment)
+
+print(airway)
+print(colnames(airway))
+print(head(assay(airway)))
+print(colData(airway))
