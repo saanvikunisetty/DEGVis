39e41e60540d4e8cf211f9d1adc9bd3cfafe8026
Saanvi Kunisetty <4saanvik@gmail.com>
2025-06-03 19:01:07 -0500

    Filtered Out Low-Count Genes, Categorized Treatment and Cell Type Data

--- degvis.R
+++ degvis.R
@@ -14,8 +14,20 @@
 
 data("airway")
 library(SummarizedExperiment)
+#airway dataset
+#how human airway smooth muscle responds to DEX (asthma medication)
 
 print(airway)
 print(colnames(airway))
 print(head(assay(airway)))
 print(colData(airway))
+#initial data inspection
+
+keep_genes <- rowSums(assay(airway)) >= 10
+airway_filtered <- airway[keep_genes, ]
+#filter out any genes with really low total sample counts
+colData(airway_filtered)$dex <- as.factor(colData(airway_filtered)$dex)
+colData(airway_filtered)$cell <- as.factor(colData(airway_filtered)$cell)
+#treat different sample types as categories
+dim(airway)         
+dim(airway_filtered)
