30035f304381cf4c8b4f1b8d333575789a013f71
Saanvi Kunisetty <4saanvik@gmail.com>
2025-06-05 12:00:19 -0500

    Identified Top Differentially Expressed Genes by Adjusted p-values

--- degvis.R
+++ degvis.R
@@ -5,11 +5,11 @@
 #    install.packages("BiocManager")
 
 BiocManager::install("DESeq2", force = TRUE)
-#BiocManager::install("airway", force = TRUE)
+BiocManager::install("airway", force = TRUE)
 
 #library(tidyverse)
 library(DESeq2)
-#library(airway)
+library(airway)
 #library(pheatmap)
 
 data("airway")
@@ -34,4 +34,14 @@ dim(airway_filtered)
 #overall data cleaning
 
 dds <- DESeqDataSet(airway_filtered, design = ~ cell + dex)
+#initializing the dataset
 dds <- DESeq(dds)
+#run the pipeline
+
+res <- results(dds, contrast = c("dex", "trt", "untrt"))
+#extract results from dds object
+#compare/contrast treated vs untreated (which genes are different between both?)
+resOrdered <- res[order(res$padj), ]
+print(head(resOrdered))
+#sort from smallest to largest p-value
+#determine most certain differentially expressed genes
